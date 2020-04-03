7428738f02b3d54c54a937fd2e3949d6fe58d969
shri <shrija.tumma1994@gmail.com>
2020-04-03 14:28:25 +0530

    Modify broadcast messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/create'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'chatroom#index'
@@ -9,4 +7,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 end
