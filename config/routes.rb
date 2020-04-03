fbd2ffdb467e095fbb10d814817446ea0608a92f
shri <shrija.tumma1994@gmail.com>
2020-04-03 12:24:03 +0530

    Add messages from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'messages/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'chatroom#index'
@@ -6,4 +8,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 end
